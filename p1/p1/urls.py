9ea811b2e8bcc4cb66e6b4d8478ae47eed1a62eb
Shakil Ahmed <shakilahmed31@gmail.com>
2021-10-17 21:23:13 +0600

    Django P1 commit

--- p1/p1/urls.py
+++ p1/p1/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from testapp import views
-from testapp.views import Test_func1
+from testapp.views import Test_func1 ,Test_func2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Test_func),
     path('Test_func1/', Test_func1),
+    path('Test_func2/',views.Test_func2),
+    path('add/',views.add),
 ]
--- p1/testapp/views.py
+++ p1/testapp/views.py
@@ -9,3 +9,10 @@ def Test_func(request):
 def Test_func1(request):
   return HttpResponse("<h1>testing function call directly from Urls,structure is like that 'from testapp.views import Test_func1,path('Test_func1/', Test_func1),</h1>")
 
+def Test_func2(request):
+  a="<h2> Here under this project , writing some function to views and call different ways </h2>"
+  return HttpResponse(a)
+
+def add(request):
+  a=15+20
+  return HttpResponse(a)
